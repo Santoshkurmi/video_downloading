ab23e8882084e9476e1066879c78505c2de55038
Santosh kurmi <xantosh121@gmail.com>
2025-03-07 15:20:21 +0545

    added to download video only

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import requests as req 
 import os
-from Crypto.Cipher import AES   #pip install pycryptodome
-from Crypto.Util.Padding import unpad
 import base64
 import re
 import time
@@ -10,7 +8,6 @@ import base64
 import hashlib
 from base64 import b64decode
 from Crypto.Cipher import AES
-import m3u8
 import requests
 import os
 import threading
@@ -20,36 +17,47 @@ import mmap
 import sys
 
 
-user_id = "74312"
-authorization = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6Ijc0MzEyIiwiZW1haWwiOiJraHVtYXBva2hhcmVsMjA3OEBnbWFpbC5jb20iLCJ0aW1lc3RhbXAiOjE3MzI2NDIyNzR9.Wz2iUOpyMzmuo_bi0PV-eu7JgnVVHFXj3PS4SagtmYQ"
+user_id = "582457"
+authorization = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjU4MjQ1NyIsImVtYWlsIjoidmlza3VtYXJiaXQwMDFAZ21haWwuY29tIiwidGltZXN0YW1wIjoxNzQxMzMwMDcxLCJ0ZW5hbnRUeXBlIjoidXNlciIsInRlbmFudE5hbWUiOiIiLCJ0ZW5hbnRJZCI6IiIsImRpc3Bvc2FibGUiOmZhbHNlfQ.86oVW0qvAg2bd65MYlhwBVEwu4r96iqiBtotif3w68k"
 
-host = "https://harkiratapi.classx.co.in"
-
-
-
+host = "https://parmaracademyapi.classx.co.in"
 
 headers = {
-    "Authorization":authorization,
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.86 Safari/537.36",
-    "Origin": "https://harkirat.classx.co.in",
-    "Host": "harkiratapi.classx.co.in",
-    "Sec-Ch-Ua-Platform": "Linux",
-    "Referer": "https://harkirat.classx.co.in/",
-    "Auth-Key": "appxapi"
+    "Authorization": authorization,
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+    "Origin": "https://www.parmaracademy.in",
+    "Referer": "https://www.parmaracademy.in/",
+    "Sec-Ch-Ua-Platform": "Windows",
+    "Auth-Key": "appxapi",
+    "Client-Service": "Appx",
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br"
 }
 
 def get_all_purchases():
-    res = req.get(host+f"/get/get_all_purchases?userid={user_id}&item_type=10",headers=headers).json()
+    # res = req.get(host+f"/get/get_all_purchasesv2?userid={user_id}&item_type=1",headers=headers).json()
+    res = req.get(host+f"/get/courselist?exam_name=&start=0",headers=headers).json()
     res = res["data"]
     return res
 
 def get_titles(id,pid=-1):
-    res = req.get(host+f"/get/folder_contentsv2?course_id={id}&parent_id={pid}",headers=headers).json()
+    res = req.get(host+f"/get/allsubjectfrmlivecourseclass?courseid={id}&start={pid}",headers=headers).json() 
     res = res["data"]
     return res
 
+def get_titles_of_toipic(id,pid):
+    res = req.get(host+f"/get/alltopicfrmlivecourseclass?courseid={id}&subjectid={pid}&start=-1",headers=headers).json() 
+    res = res["data"]
+    return res
+
+def get_all_video_links(id,pid, tid):
+    res = req.get(host+f"/get/livecourseclassbycoursesubtopconceptapiv3?courseid={id}&subjectid={pid}&topicid={tid}&conceptid=&windowsapp=false&start=-1",headers=headers).json()
+    res = res["data"]
+    return res
+  
 def get_video_token(cid,vid):
-    res = req.get(host+f"/get/fetchVideoDetailsById?course_id={cid}&video_id={vid}&ytflag=0&folder_wise_course=1",headers=headers).json()
+    res = req.get(host+f"/get/fetchVideoDetailsById?course_id={cid}&video_id={vid}&ytflag=0&folder_wise_course=0",headers=headers).json()
     token = res["data"]["video_player_token"]
     # cookie = res["data"]["cookie_value"]
     return token
@@ -68,6 +76,10 @@ def get_video_html(token):
 # for downloading video in real one
 
 
+course_name = ""
+subject_name = ""
+topic_name = ""
+video_name = ""
 
 def start():
     courses = get_all_purchases()
@@ -75,81 +87,85 @@ def start():
 
     c = 1
     for course in courses:
-        name =  course["coursedt"][0]["course_name"]
+        name =  course["course_name"]
         # id = course["itemid"]
         print(f"{c}. {name}")
         c = c + 1
     
     choice = input("\n\nEnter the course: ")
-    c_title = "courses/"+courses[int(choice)-1]["coursedt"][0]["course_name"]
-    cid = courses[int(choice)-1]["itemid"]
+    course_name = "courses/" + courses[int(choice)-1]["course_name"]
+    cid = courses[int(choice)-1]["id"] # CourseID
+ 
 
-    
+    os.makedirs(course_name, exist_ok=True)
 
-    os.makedirs(c_title, exist_ok=True)
-
-    titles = get_titles(cid)
+    titles = get_titles(cid) # get all subject of that course
     c=1
     if len(titles) !=1:
         for title in titles:
-            print(f"{c}. {title['Title']} | {title['material_type']}")
+            print(f"{c}. {title['subject_name']}")
             c = c + 1
     
-        choice = input("\nEnter the choice: ")
+        choice = input("\nEnter the subject choice: ")
+    else:
+        choice = "1"
+      
+    pid = titles[int(choice)-1]["subjectid"] # SubjectID
+    subject_name = course_name + "/" + titles[int(choice)-1]["subject_name"] 
+
+    titles = get_titles_of_toipic(cid, pid) # get all topic of that subject
+
+    # topicID
+    c=1
+    if len(titles) !=1:
+        for title in titles:
+            print(f"{c}. {title['topic_name']}")
+            c = c + 1
+    
+        choice = input("\nEnter the toipc choice: ")
     else:
         choice = "1"
 
-    pid = titles[int(choice)-1]["id"]
+    tid = titles[int(choice)-1]["topicid"] # toipcID
+    topic_name = subject_name + "/" + titles[int(choice)-1]["topic_name"]
 
-    titles = get_titles(cid,pid)
 
 
-    choice= input("Choose Download options: \n1. Links only\n2. Download Links and Videos both\n=>")
+    videos = get_all_video_links(cid, pid, tid)
 
+    os.makedirs(topic_name, exist_ok=True)
+
+    for idx, video in enumerate(videos):
+
+        if video['material_type'] !="VIDEO":
+            print("Ignoring this file as it is not video")
+            continue
+
+        print(f"{idx+1}: {video['Title']} - {video['id']}")
+        new_video_title = re.sub(r'\W', '', video['Title'])
+
+        token = get_video_token(cid, video['id'])
+        video_name = topic_name  + "/" +  new_video_title + ".html"
     
+        if os.path.exists(video_name):
+          print("Already downloaded link for "+"'"+new_video_title+"'\n")
+          continue
 
+        video_html = get_video_html(token)
+        video_html = video_html.replace('src="/','src="https://www.parmaracademy.in/')
+        video_html = video_html.replace('href="/','href="https://www.parmaracademy.in/')
+        video_html = video_html.replace('"quality":"360p","isPremier":','"quality":"720p","isPremier":')
 
-    if True:
-
-        c=1
-        for title in titles:
-            print(f"\n{c}. Downloading link of '{title['Title']} | {title['material_type']}'")
-            vid = title["id"]
-            if title['material_type'] !="VIDEO":
-                print("Ignoring this file as it is not video")
-                continue
-            c = c + 1
-
-            if os.path.exists(c_title+"/"+title["Title"]+".html"):
-                print("Already downloaded link for "+"'"+title["Title"]+"'\n")
-                continue
-
-            # exit()
+        if "Token Expired" in video_html:
+            print("This one is expired...\n")
+            # print("Waiting for 30 seconds to prevent rate limiting\n")
+            # time.sleep(30)
+            continue
         
-            # choice = input("Enter the choice: ")
-            # vid = titles[int(choice)-1]["id"]
+        with open(video_name,"w") as e:
+            e.write(video_html)
 
-            vtoken = get_video_token(cid,vid)
-            # print(vtoken)
-            # print(cookie)
-
-            html = get_video_html(vtoken)
-            html = html.replace('src="/','src="https://player.akamai.net.in/')
-            html = html.replace('href="/','href="https://player.akamai.net.in/')
-            html = html.replace('"quality":"360p","isPremier":','"quality":"720p","isPremier":')
-
-            # print(c_title)
-            if "Token Expired" in html:
-                print("This one is expired...\n")
-                print("Waiting for 30 seconds to prevent rate limiting\n")
-                time.sleep(30)
-                continue
-
-            with open(c_title+"/"+title["Title"]+".html","w") as e:
-                e.write(html)
-
-            print("Waiting for 30 seconds to prevent rate limiting\n")
-            time.sleep(30)
-
+        # print("Waiting for 30 seconds to prevent rate limiting\n")
+        # time.sleep(30)
 
 start()
